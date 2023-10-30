9f1dc5dd2bc4af390832fb58b1379572a4016f44
usaidahmed01 <usaid423@gmail.com>
2023-10-30 18:55:34 +0500

    my assignment

---
+++ app.js
@@ -0,0 +1,560 @@
+// Alert : 01
+
+// alert('Hello JS')
+// alert('Error! Please enter a valid password')
+// alert('Hello JS land'+ '\n' + 'Happy Coding') 
+// alert('Hello JS land')
+// alert('Happy Coding')
+// alert('I can run JS through my web browser console') 
+
+
+// Variable For Strings : 02
+
+
+// const myName = 'Usaid Ahmed'
+// alert (myName)
+
+// const myUni = 'NED'
+// alert (myUni)
+
+
+// const myCourse = 'Mobile & Cloud Computing - Module A'
+// alert (myCourse)
+
+// const dream = 'You will be a Mobile App Developer'
+// alert (dream)
+
+// const email = 'usaid423@gmail.com'
+// alert ('My email address is '+ email)
+
+// const aBook = 'A Smarter way to learn java script'
+// alert ('I am trying to learn from the book ' + aBook)
+
+
+
+// Variables For Numbers : 03
+
+
+
+
+
+// const age = 18
+// alert ('I am ' + age + ' Years old')
+
+
+
+
+// const visit = 2
+// alert ('You have visited this site ' + visit + ' times')
+
+
+
+// const userName = prompt ('Hey!' + '\n' + 'What is Your name')
+// const proName = prompt ('Tell me what product do you want?')
+// const proQuant = prompt ('And how many?')
+// console.log('Hey! ' + userName + ' so you bought ' + proQuant +' '+ proName + '\n' + 'Thats Great!');
+
+
+
+// var name = 'usaid'
+// var name = 'usaid'
+// var name = 'usaid'
+
+
+
+// const _name = 'usaid'
+// const $name = 'usaid'
+// const place = 'Park'
+// const animalName = 'usaid'
+// const okk = 'usaid'
+
+
+
+// const 123 = 'usaid'
+// const *123 = 'usaid'
+// const %123 = 'usaid'
+// const !123 = 'usaid'
+// const #123 = 'usaid'
+
+
+
+
+// Math Expressions : 04
+
+
+
+
+// const num1 = +prompt ('Enter Number you want to add')
+// const num2 = +prompt ('Enter Number you want to add')
+// const sum = num1 + num2
+//  console.log('Sum of ' + num1 + ' and ' + num2 +' is ' + sum);
+
+
+
+
+// const num1 = +prompt ('Enter Number you want to multiply')
+// const num2 = +prompt ('Enter Number you want to multiply')
+// const mul = num1 * num2
+//  console.log('Result of multiplying' + num1 + ' and ' + num2 +' is ' + mul);
+
+
+
+// const num1 = +prompt ('Enter Number you want to Divide')
+// const num2 = +prompt ('Enter Number you want to Divide')
+// const div = num1 / num2
+//  console.log('Result of Division' + num1 + ' and ' + num2 +' is ' + div);
+
+
+
+
+// const tickRs = 600
+// const tickBuy = +prompt ('How many Tickets you wanna buy'+'\n Cost of each Ticket is 600')
+// const totalTick = tickRs * tickBuy
+// console.log('Total cost to buy ' + tickBuy + ' tickets to a movie is ' + totalTick + ' PKR.');
+
+
+
+
+// const num1 = +prompt('Enter any number for mulitiplication')
+// const mul1 = num1 * 1
+// const mul2 = num1 * 2
+// const mul3 = num1 * 3
+// const mul4 = num1 * 4
+// const mul5 = num1 * 5
+// const mul6 = num1 * 6
+// const mul7 = num1 * 7
+// const mul8 = num1 * 8
+// const mul9 = num1 * 9
+// const mul10 = num1 * 10
+
+
+
+
+
+// console.log('<table>' + '<tr>' + '<td>' + num1 + ' x ' + '1' + ' = ' + mul1 + '</td>' + '</tr>' +
+//     '<tr>' + '<td>' + num1 + ' x ' + '2' + ' = ' + mul2 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '3' + ' = ' + mul3 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '4' + ' = ' + mul4 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '5' + ' = ' + mul5 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '6' + ' = ' + mul6 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '7' + ' = ' + mul7 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '8' + ' = ' + mul8 + '</td>' + '</tr>' +
+// '<tr>' + '<td>' + num1 + ' x ' + '9' + ' = ' + mul9 + '</td>' + '</tr>' +
+//  '<tr>' + '<td>' + num1 + ' x ' + '10' + ' = ' + mul10 + '</td>' + '</tr>' + '</table>');
+
+
+
+
+
+// const temp = +prompt('Lets do some conversion')
+// const cTof = 1.8 * temp + 32
+// const fToc = temp - 32 / 1.8
+// console.log(temp + ' Celcius ' + ' = ' + cTof + ' Fahrenheit ' + '\n' + temp + ' Fahrenheit ' + ' = ' + fToc + ' Celcius ');
+
+
+
+
+
+// const prItem1 = +prompt('Enter your 1st item price')
+// const quanItem1 = +prompt('Enter quantity of 1st item')
+// const prItem2 = +prompt('Enter your 2nd item price')
+// const quanItem2 = +prompt('Enter quantity of 2nd item')
+// const totalPr1 = prItem1 * quanItem1
+// const totalPr2 = prItem2 * quanItem2
+// const ship = 200
+// const sum = totalPr1 + totalPr2 + ship
+// console.log('Price of 1st Item is ' + prItem1 + '\n' + 'Quantity of 1st Item is ' + quanItem1 + '\n'
+//     + 'Price of 2nd Item is ' + prItem2 + '\n' + 'Quantity of 2nd Item is ' + quanItem2 + '\n' + 'Shipping Charges 200' + '\n' + 'Total Cost of your order is ' + sum + 'PKR');
+
+
+
+
+
+// const name = prompt('Enter Your Name')
+// const chemMarks = +prompt('Enter Your Chemistry Marks')
+// const mathsMarks = +prompt('Enter Your Maths Marks')
+// const phyMarks = +prompt('Enter Your Physics Marks')
+// const urduMarks = +prompt('Enter Your Urdu Marks')
+// const engMarks = +prompt('Enter Your English Marks')
+// const pstMarks = +prompt('Enter Your Pak Studies Marks')
+// const obtMarks = chemMarks + mathsMarks + phyMarks + urduMarks + engMarks + pstMarks
+// const totalMarks = 550
+// const percentage = obtMarks / totalMarks * 100
+
+// document.write('Name : ' + name + '\n' + 'Total Marks : ' + totalMarks + '\n' + 'Obtained Marks : ' + obtMarks +
+//     '\n' + name + ' achieved ' + percentage + ' %.')
+// console.log('Name : ' + name + '\n' + 'Total Marks : ' + totalMarks + '\n' + 'Obtained Marks : ' + obtMarks +
+//     '\n' + name + ' achieved ' + percentage + ' %.')
+
+
+
+
+
+
+// const usd = +prompt ('Enter Price in USD')
+// const riyal = +prompt ('Enter Price in Riyal')
+// const usdTopkr = usd * 276
+// const riyalToPKR = riyal * 73
+// const total = usdTopkr + riyalToPKR
+// console.log('Your Total Amount in PKR is ' + total);
+
+
+
+
+
+// const num = +prompt ('Enter any Number')
+// const numAdd = +prompt ('Enter any number for Addition')
+// const numMul = +prompt ('Enter any number for Multiplication')
+// const numDiv = +prompt ('Enter any number for Division')
+// const step1 = num + numAdd * numMul / numDiv
+// console.log(step1);
+
+
+
+
+
+
+
+// const birthYear = +prompt ('Enter Your year of Birth')
+// const age1 =  2023 - birthYear
+// const age2 =  2024 - birthYear
+
+//  console.log('He/She is either ' + age1 + ' Or ' + age2 );
+
+
+
+
+
+
+
+// const radius = +prompt ('Enter the value of Radius')
+// const circumference = radius * 2 * 3.142
+// const area = radius * radius * 3.142
+// function result(radius , circumference , area){
+
+// console.log('Radius of a circle is ' + radius);
+// console.log('Circumference of a circle is ' + circumference);
+// console.log('Area of a circle is ' + area);
+// }
+// result(radius , circumference , area)
+
+
+
+
+// const fav = prompt ('Enter your favourite food')
+// const age = +prompt ('Enter your age')
+// const perDay = +prompt ('How many ' + fav + ' you eat per day')
+// const total = 70 - age
+// const total1 = total * perDay
+// console.log('Your Favourite food is ' + fav + '\n' + 'Your Age is ' + age + '\n' + 'Estimated age is 70' + '\n' + 'Fav food you eat per day : ' + perDay  + '\n' + 'You will need ' + total1 + ' ' + fav +' to last you until the ripe old age of 70');
+
+
+
+
+
+
+// Assignment 
+
+// const userName = prompt('Enter Your Name')
+// const pro1 = prompt('1st Product Name')
+// const pro2 = prompt('1st Product Name')
+// const pro1Price = +prompt('Price of 1st Product')
+// const pro2Price = +prompt('Price of 2nd Product')
+// const sum = pro1Price + pro2Price
+
+// function info(userName, pro1, pro2, pro1Price, pro2Price, sum) {
+//     return ' Your Name is ' + userName + ' Product 1 Name is ' + pro1 + ' Product 2 Name is ' + pro2 + ' Price of 1st Product is ' + pro1Price + ' Price of 2nd Product is ' + pro2Price + ' Your Total Amount is ' + sum
+// }
+// console.log(info(userName, pro1, pro2, pro1Price, pro2Price, sum));
+
+
+
+
+// Concating : 06
+
+
+
+// const num1 = +prompt('Enter any number for mulitiplication')
+// const mul1 = num1 * 1
+// const mul2 = num1 * 2
+// const mul3 = num1 * 3
+// const mul4 = num1 * 4
+// const mul5 = num1 * 5
+// const mul6 = num1 * 6
+// const mul7 = num1 * 7
+// const mul8 = num1 * 8
+// const mul9 = num1 * 9
+// const mul10 = num1 * 10
+
+
+
+
+
+// console.log('<table>' + '<tr>' + '<td>' + num1 + ' x ' + '1' + ' = ' + mul1 + '</td>' + '</tr>' +
+//     '<tr>' + '<td>' + num1 + ' x ' + '2' + ' = ' + mul2 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '3' + ' = ' + mul3 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '4' + ' = ' + mul4 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '5' + ' = ' + mul5 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '6' + ' = ' + mul6 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '7' + ' = ' + mul7 + '</td>' + '</tr>' + '<tr>' + '<td>' + num1 + ' x ' + '8' + ' = ' + mul8 + '</td>' + '</tr>' +
+// '<tr>' + '<td>' + num1 + ' x ' + '9' + ' = ' + mul9 + '</td>' + '</tr>' +
+//  '<tr>' + '<td>' + num1 + ' x ' + '10' + ' = ' + mul10 + '</td>' + '</tr>' + '</table>');
+
+
+
+
+
+
+
+
+// const city = prompt ('Enter any City of Pakistan')
+// if (city == 'karachi'){
+//     console.log('Welcome to the City of Lights ');
+// }else if (city == 'lahore'){
+//     console.log('Welcome to the City of Historic places ');
+
+// }else {
+//     console.log('Sorry cannot recognise your city');
+// }
+
+
+
+
+
+
+
+
+// const gender = prompt ('Enter your Gender : '+'\n' + 'Male or Female')
+// if (gender == 'Male') {
+//     console.log('Good Morning Sir :)');
+
+// }else {
+//     console.log('Good Morning Maam :)');
+
+// }
+
+
+
+
+
+
+
+// const signal = prompt ('Choose any color'+'\n'+'Red'+'\n'+'Yellow'+'\n'+'Green')
+// if (signal == 'Red') {
+//     console.log('vehicles must stop');
+// }else if (signal == 'Yellow'){
+//     console.log('vehicles should get ready to  move');
+// }else {
+//     console.log('vehicles can move now');
+// }
+
+
+
+
+
+
+// const age = +prompt ('Enter your age')
+// if (age >= 18) {
+//     console.log('You are Welcome');
+// }else{
+//     console.log('Warning! this website is only for 18+');
+// }
+
+
+
+
+
+
+// const fuel = +prompt ('Enter your car fuel (in litres)')
+// if (fuel < 0.25) {
+//     console.log('Please refill the fuel in your car ');
+
+// }else {
+//     console.log('Have a safe ride :)');
+// }
+
+
+
+
+
+
+// const chemMarks = +prompt('Enter Your Chemistry Marks out of 100')
+// const mathsMarks = +prompt('Enter Your Maths Marks out of 100')
+// const phyMarks = +prompt('Enter Your Physics Marks out of 100')
+// const urdMarks = +prompt('Enter Your Urdu Marks out of 100')
+// const engMarks = +prompt('Enter Your English Marks out of 100')
+// const pstMarks = +prompt('Enter Your PST Marks out of 50')
+
+// const sum = chemMarks + mathsMarks + phyMarks + urdMarks + engMarks + pstMarks
+// const percentage = sum / 550 * 100
+// if (percentage >= 80) {
+//     console.log('Total Marks: 300'+'\n'+'Marks Obtained '+ sum +'\n'+'Percentage : '+ percentage +'%'+'\n'+'Grade : A-1' + '\n' + 'Remarks : Excellent!');
+
+// }else if (percentage >=70){
+//     console.log('Total Marks: 300'+'\n'+'Marks Obtained '+ sum +'\n'+'Percentage : '+ percentage +'%'+'\n'+'Grade : A' + '\n' + 'Remarks : Good!');
+
+// }else if(percentage >=60){
+//     console.log('Total Marks: 300'+'\n'+'Marks Obtained '+ sum +'\n'+'Percentage : '+ percentage +'%'+'\n'+'Grade : B' + '\n' + 'Remarks : You need to improve');
+
+// }else {
+//     console.log('Total Marks: 300'+'\n'+'Marks Obtained '+ sum +'\n'+'Percentage : '+ percentage +'%'+'\n'+'Grade : Fail' + '\n' + 'Remarks : Sorry');
+
+// }
+
+
+
+
+
+
+
+// const nameofitem1 = prompt ('Enter Item no. 1')
+// const nameofitem2 = prompt ('Enter Item no. 2')
+// const quanItem1 = +prompt ('Enter Quanity of ' + nameofitem1)
+// const quanItem2 = +prompt ('Enter Quanity of ' + nameofitem2)
+// const prItem1 = +prompt ('Enter the Price of ' + nameofitem1)
+// const prItem2 = +prompt ('Enter the Price of ' + nameofitem2)
+// const calprOfItem1 = prItem1 *quanItem1
+// const calprOfItem2 = prItem2 *quanItem2
+// const shipcost = 250
+// const total = calprOfItem1 + calprOfItem2 + shipcost
+// const dis10 = total - 10/100
+
+// if (total > 2000) {
+//     console.log( 'Price of '+ nameofitem1 + ' is ' + prItem1 +'\n'+'Quantity of '+ nameofitem1 +' is '+ quanItem1 +'\n'+'Price of '+ nameofitem2 + ' is ' + prItem2 +'\n'+'Quantity of '+ nameofitem2 + ' is '+ quanItem2 + '\n'+ 'Shipping Charges : '+ shipcost +'\n' +'Total cost of your order is ' + total + '\n' + 'After 10% Discount your amount is ' + dis10);
+
+// }else{
+//     console.log( 'Price of '+ nameofitem1 + ' is ' + prItem1 +'\n'+'Quantity of '+ nameofitem1 +' is '+ quanItem1 +'\n'+'Price of '+ nameofitem2 + ' is ' + prItem2 +'\n'+'Quantity of '+ nameofitem2 + ' is '+ quanItem2 + '\n'+ 'Shipping Charges : '+ shipcost +'\n' +'Total cost of your order is ' + total);
+
+// }
+
+
+
+
+
+
+// const secNum = 7
+// const gNum = +prompt ('Guess the secret number')
+// const gNum2  = gNum +1
+// const gNum3  = gNum -1
+
+// if ( gNum === secNum) {
+//     console.log('Bingoo!, Correct Answer');
+
+// } else if ( gNum2 == secNum ){
+//     console.log('Close enough to the correct answer');
+
+// } else if ( gNum3 == secNum ){
+//     console.log('Close enough to the correct answer');
+
+// }else {
+//     console.log('Noo! Wrong Answer');
+// }
+
+
+
+
+
+
+
+// const num1 = +prompt('Enter any number')
+// const num1div3 = num1 / 3
+// if (num1 == 3 || num1 == 6 || num1 == 9  || num1 == 12 || num1 == 15 || num1 == 18 || num1 == 21 || num1 == 24 || num1 == 27 || num1 == 30    ) {
+//     console.log(num1 +' is Divisible by 3 and the answer is '+ num1div3);
+// } else {
+//     console.log(num1 + ' is not Divisible by 3');
+
+// }
+
+
+
+
+
+
+// const name1 = prompt('Enter Team 1')
+// const name2 = prompt('Enter Team 2')
+// const score1 = +prompt ('Enter Team 1 score')
+// const score2 = +prompt ('Enter Team 2 score')
+// if (score1 > score2) {
+//     console.log(name1 + ' have won the game');
+
+// }else if (score1 == score2) {
+//     console.log('Match Drawn');
+
+// }else {
+//     console.log(name2 + ' have won the game');
+
+// }
+
+
+
+
+
+
+// const string ='Pakistan is string'
+// const num = 92 
+// const boolean = true 
+// const user = prompt ('Choose any of them : Pakistan , 92 , True')
+// if (user == 'Pakistan') {
+//     console.log(string);
+
+// }else if (user == '92'){
+//     console.log(num + 'is the number');
+// }else { 
+//     console.log(boolean + 'is bioolean');
+// }
+
+
+
+
+
+
+
+// const num = +prompt('Enter Even or Odd number')
+// if (num == 2 || num == 4 || num == 6 || num == 8 || num == 10 || num == 12 || num == 14 || num == 16 || num == 18 || num == 20 || ) {
+//    console.log( num + ' is Even number');
+
+// }else {
+//    console.log( num + ' is Odd number');
+
+// }
+
+
+
+
+
+
+
+// const temp = +prompt('Enter the temperature of your home')
+// if (temp >= 40) {
+//     console.log('Its too hott!');
+// } else if (temp >= 30) {
+//     console.log('The weather today is Normal');
+
+// }else if (temp >= 20){
+//     console.log('Todays weather is Cool');
+
+// }else {
+//     console.log('OMG! Todays weather is so Cool');
+
+// }
+
+
+
+
+
+
+// const num1 = +prompt('Enter number 1')
+// const num2 = +prompt('Enter number 1')
+// const opr = prompt('+ , - , * , / , %')
+// const add = num1 + num2
+// const sub = num1 - num2
+// const mul = num1 * num2
+// const div = num1 / num2
+// const mod = num1 % num2
+
+// if (opr == '+') {
+//     console.log(num1 + ' + ' + num2 + ' = ' + add);
+    
+// }else if (opr == '-'){
+//     console.log(num1 + ' - ' + num2 + ' = ' + sub);
+
+// }else if (opr == '*'){
+//     console.log(num1 + ' * ' + num2 + ' = ' + mul);
+
+
+// }else if (opr == '/'){
+//     console.log(num1 + ' / ' + num2 + ' = ' + div);
+
+// }else {
+//     console.log(num1 + ' % ' + num2 + ' = ' + mod);
+
+// }
